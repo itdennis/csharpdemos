cf120d245a0ed7f164194d183e207fd8b2c109f3
dennis wu <15040554157@163.com>
2020-04-23 21:36:21 +0800

    update at 2020-4-23 21:36:19

---
+++ demo01/Delegate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace demo01
+{
+    public class Delegate : IDemoBase
+    {
+        public void Run()
+        {
+            new Action(() =>
+            {
+                Console.WriteLine("666");
+            }).SafeInvoke();
+        }
+    }
+}
---
+++ demo01/DelegateAdvance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace demo01
+{
+    public static class DelegateAdvance
+    {
+        public static void SafeInvoke(this Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        public static void SafeSaveChange(this Func<string> func)
+        {
+            var res = func.Invoke();
+            if (true)
+            {
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
